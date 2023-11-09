ef0b2a73887351dc45b83cb495863504a11f95f6
RockeyWereWolf <mi71k9d7p@code.edu.az>
2023-11-09 19:48:20 +0400

    updated structure

--- TeamPizzaTask/Databases/ProductsDatabase.cs
+++ TeamPizzaTask/Databases/ProductsDatabase.cs
@@ -4,11 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace TeamPizzaTask.Enums
+namespace TeamPizzaTask.Databases
 {
-    internal enum RoleEnum
+    internal static class ProductsDatabase
     {
-        User,
-        AdminUser
     }
 }
